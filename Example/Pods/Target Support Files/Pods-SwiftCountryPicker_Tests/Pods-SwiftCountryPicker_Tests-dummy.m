4fd31740f8481976362f7a9b3fb4446035d5a655
Samuel Beek <ik@samuelbeek.com>
2015-11-28 16:23:00 +0100

    Set up cocoapods

---
+++ Example/Pods/Target Support Files/Pods-SwiftCountryPicker_Tests/Pods-SwiftCountryPicker_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftCountryPicker_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftCountryPicker_Tests
+@end
---
+++ Example/Pods/Target Support Files/SwiftCountryPicker/SwiftCountryPicker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftCountryPicker : NSObject
+@end
+@implementation PodsDummy_SwiftCountryPicker
+@end
