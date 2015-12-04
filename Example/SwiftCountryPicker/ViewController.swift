6831d31c4c52b9b7a33f417a29960e3608955b24
Samuel Beek <ik@samuelbeek.com>
2015-12-04 11:16:53 +0100

    Make example more clear

--- Example/SwiftCountryPicker/ViewController.swift
+++ Example/SwiftCountryPicker/ViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 import SwiftCountryPicker
 
 class ViewController: UIViewController {
+    
+    var textField : UITextField! {
+        didSet {
+            textField.center = view.center
+            textField.textAlignment = .Center
+            textField.layer.borderColor = UIColor.orangeColor().CGColor
+            textField.layer.borderWidth = 2.0
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let textField = UITextField(frame: CGRectMake(0,0,200,50))
-        textField.center = view.center
+        textField = UITextField(frame: CGRectMake(0,0,40,30))
         view.addSubview(textField)
         
-        let countryPicker = CountryPicker(frame: CGRectMake(0,self.view.frame.size.height-216,self.view.frame.size.width, 216))
+        let countryPicker = CountryPicker(frame: CGRectMake(0,0,self.view.frame.size.width, 216))
         countryPicker.countryDelegate = self
         textField.inputView = countryPicker
         
@@ -34,7 +42,7 @@ class ViewController: UIViewController {
 
 extension ViewController : CountryPickerDelegate {
     func countryPicker(picker: CountryPicker, didSelectCountry country: Country) {
-        print(country.emoji)
+        textField.text = country.iso
     }
 
 }
